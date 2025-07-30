bfd5eadd24e810f76d143d6f16653812422487b5
swapnil2000 <swapnilsharma0220@gmail.com>
2025-07-30 22:31:54 +0530

    agent integrated

--- app/(root)/interview/page.tsx
+++ app/(root)/interview/page.tsx
@@ -1,14 +1,22 @@
-import Agent from '@/components/Agent'
-import React from 'react'
+import Agent from "@/components/Agent";
+import { getCurrentUser } from "@/lib/actions/auth.action";
 
-const Page = () => {
+const Page = async () => {
+  const user = await getCurrentUser();
+
   return (
-      <>
-          <h3>Interview Generation</h3>
+    <>
+      <h3>Interview generation</h3>
 
-          <Agent userName='You' userId='user 1' type='generate'/>
-      </>
-  )
-}
+      <Agent
+        // eslint-disable-next-line @typescript-eslint/no-non-null-asserted-optional-chain
+        userName={user?.name!}
+        userId={user?.id}
+        // profileImage={user?.profileURL}
+        type="generate"
+      />
+    </>
+  );
+};
 
-export default Page
+export default Page;
