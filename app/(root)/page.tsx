57c7dd480c05bc41eabcfcb40c3d8eab979b1326
swapnil2000 <swapnilsharma0220@gmail.com>
2025-08-01 17:46:38 +0530

    Show generated interview in the dashboard

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -6,8 +6,23 @@ import Link from 'next/link';
 import Image from 'next/image';
 import { dummyInterviews } from '@/constants';
 import InterviewCard from '@/components/InterviewCard';
+import {
+	getCurrentUser,
+	getInterviewsByUserId,
+	getLatestInterviews,
+} from '@/lib/actions/auth.action';
 
-const page = () => {
+const page = async () => {
+	const user = await getCurrentUser();
+
+	const [userInterviews, latestInterviews] = await Promise.all([
+		await getInterviewsByUserId(user?.id!),
+		await getLatestInterviews({ userId: user?.id! }),
+	]);
+
+	const hasPastInterviews = userInterviews?.length > 0;
+	const hasUpcomingInterviews = latestInterviews?.length > 0;
+
 	return (
 		<>
 			<section className='card-cta'>
@@ -33,18 +48,31 @@ const page = () => {
 			<section className='flex flex-col gap-6 mt-8'>
 				<h2>Your Interview</h2>
 				<div className='interviews-section'>
-					{dummyInterviews.map((interview) => (
-						<InterviewCard {...interview} key={interview.id} />
-					))}
-					{/* <p>You haven&apos;t taken any interviews.</p> */}
+					{hasPastInterviews ? (
+						userInterviews?.map((interview) => (
+							<InterviewCard
+								{...interview}
+								key={interview.id}
+							/>
+						))
+					) : (
+						<p>You haven&apos;t taken any interviews.</p>
+					)}
 				</div>
 			</section>
 			<section className='flex flex-col gap-6 mt-8'>
-				<h2>Taken Interview</h2>
+				<h2>Take an Interview</h2>
 				<div className='interviews-section'>
-					{dummyInterviews.map((interview) => (
-						<InterviewCard {...interview} key={interview.id} />
-					))}
+					{hasUpcomingInterviews ? (
+						latestInterviews?.map((interview) => (
+							<InterviewCard
+								{...interview}
+								key={interview.id}
+							/>
+						))
+					) : (
+						<p>There are no new interviews available.</p>
+					)}
 				</div>
 			</section>
 		</>
--- lib/actions/auth.action.ts
+++ lib/actions/auth.action.ts
@@ -87,37 +87,70 @@ export async function setSessionCookie(idToken: string) {
 	});
 }
 
-
 export async function getCurrentUser(): Promise<User | null> {
-    const cookieStore = await cookies();
+	const cookieStore = await cookies();
 
-    const sessionCookie = cookieStore.get('session')?.value;
+	const sessionCookie = cookieStore.get('session')?.value;
 
-    if(!sessionCookie) return null;
+	if (!sessionCookie) return null;
 
-    try {
-        const decodedClaims = await auth.verifySessionCookie(sessionCookie, true);
+	try {
+		const decodedClaims = await auth.verifySessionCookie(sessionCookie, true);
 
-        const userRecord = await db.
-            collection('users')
-            .doc(decodedClaims.uid)
-            .get();
+		const userRecord = await db
+			.collection('users')
+			.doc(decodedClaims.uid)
+			.get();
 
-        if(!userRecord.exists) return null;
+		if (!userRecord.exists) return null;
 
-        return {
-            ...userRecord.data(),
-            id: userRecord.id,
-        } as User;
-    } catch (e) {
-        console.log(e)
+		return {
+			...userRecord.data(),
+			id: userRecord.id,
+		} as User;
+	} catch (e) {
+		console.log(e);
 
-        return null;
-    }
+		return null;
+	}
 }
 
 export async function isAuthenticated() {
-    const user = await getCurrentUser();
+	const user = await getCurrentUser();
 
-    return !!user;
+	return !!user;
+}
+
+export async function getInterviewsByUserId(
+	userId: string
+): Promise<Interview[] | null> {
+	const interviews = await db
+		.collection('interviews')
+		.where('userId', '==', userId)
+		.orderBy('createdAt', 'desc')
+		.get();
+
+	return interviews.docs.map((doc) => ({
+		id: doc.id,
+		...doc.data(),
+	})) as Interview[];
+}
+
+export async function getLatestInterviews(
+	params: GetLatestInterviewsParams
+): Promise<Interview[] | null> {
+	const { userId, limit = 20 } = params;
+
+	const interviews = await db
+		.collection('interviews')
+		.orderBy('createdAt', 'desc')
+		.where('finalized', '==', true)
+		.where('userId', '!=', userId)
+		.limit(limit)
+		.get();
+
+	return interviews.docs.map((doc) => ({
+		id: doc.id,
+		...doc.data(),
+	})) as Interview[];
 }
