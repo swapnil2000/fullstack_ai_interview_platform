34ee01f1f3c38598dbb256be461e726ab19469e4
swapnil2000 <swapnilsharma0220@gmail.com>
2025-08-06 23:34:08 +0530

    Deployement test

--- components/Agent.tsx
+++ components/Agent.tsx
@@ -116,7 +116,7 @@ const Agent = ({
 
   const handleCall = async () => {
     setCallStatus(CallStatus.CONNECTING);
-
+//deployement test
     if (type === "generate") {
       await vapi.start(process.env.NEXT_PUBLIC_VAPI_WORKFLOW_ID!, {
         variableValues: {
